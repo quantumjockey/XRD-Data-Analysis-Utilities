b6bf16a14e6eb8c24547fbb61e9f35e043c9dd4e
Nicola DiPalma <nickyd242@gmail.com>
2014-10-05 22:54:25 -0700

    Created dummy object for image export.

---
+++ src/xrdtiffoperations/readers/filewrappers/TiffWriter.java
@@ -0,0 +1,18 @@
+package xrdtiffoperations.readers.filewrappers;
+
+import xrdtiffoperations.imagemodel.martiff.MARTiffImage;
+
+/**
+ * Created by quantumjockey on 10/5/14.
+ */
+public class TiffWriter {
+
+    public TiffWriter(MARTiffImage dataSource){
+
+    }
+
+    public void Write(String path){
+
+    }
+
+}
