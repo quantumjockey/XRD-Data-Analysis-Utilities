2f26736d6fcdfbc5c1991f001a7000060147fae4
Nicola DiPalma <nickyd242@gmail.com>
2014-10-05 22:50:06 -0700

    Created MARTiffImage subtraction object.

---
+++ src/app/ImageManipulation/math/DataSubtractor.java
@@ -0,0 +1,38 @@
+package app.ImageManipulation.math;
+
+import xrdtiffoperations.imagemodel.martiff.MARTiffImage;
+
+public class DataSubtractor {
+
+    public static MARTiffImage SubtractImages(MARTiffImage firstImage, MARTiffImage secondImage, boolean isAbsolute){
+
+        String filename = firstImage.getFilename().replace('.', '-') + "_minus_" + secondImage.getFilename().replace('.', '-') + ".tiff";
+        MARTiffImage temp = new MARTiffImage(filename);
+        temp.ifdListing = firstImage.ifdListing;
+        int height, width;
+
+        height = (firstImage.intensityMap.length < secondImage.intensityMap.length) ? firstImage.intensityMap.length : secondImage.intensityMap.length;
+        width  = (firstImage.intensityMap[0].length < secondImage.intensityMap[0].length) ? firstImage.intensityMap[0].length : secondImage.intensityMap[0].length;
+
+        temp.intensityMap = new short[height][];
+
+        for (int x = 0; x < width; x++) {
+            temp.intensityMap[x] = new short[width];
+            for (int y = 0; y < height; y++) {
+                temp.intensityMap[x][y] = SubtractIntensity(firstImage.intensityMap[x][y], secondImage.intensityMap[x][y], isAbsolute);
+            }
+        }
+        return temp;
+    }
+
+    private static short SubtractIntensity(short firstValue, short secondValue, boolean absolute){
+        short value;
+        if (absolute){
+            value = (short) Math.abs(firstValue - secondValue);
+        } else {
+            value = (short)(firstValue - secondValue);
+        }
+        return value;
+    }
+
+}
