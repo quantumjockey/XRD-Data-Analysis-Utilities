ee9d392cc38e2fed931e12636ad1ae83108f2e89
Nicola DiPalma <nickyd242@gmail.com>
2014-10-13 16:07:20 -0700

    Created skeleton for value adjustment controls.

---
+++ src/app/valueadjuster/ValueAdjusterController.java
@@ -0,0 +1,31 @@
+package app.valueadjuster;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ValueAdjusterController {
+
+    /////////// Fields //////////////////////////////////////////////////////////////////////
+
+    @FXML
+    private TextField value;
+
+    /////////// Constructors ////////////////////////////////////////////////////////////////
+
+    public ValueAdjusterController(){
+
+    }
+
+    /////////// Public Methods //////////////////////////////////////////////////////////////
+
+    @FXML
+    public void Decrement(){
+
+    }
+
+    @FXML
+    public void Increment(){
+
+    }
+
+}
