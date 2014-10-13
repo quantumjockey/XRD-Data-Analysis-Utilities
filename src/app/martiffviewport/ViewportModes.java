3efb9e799280b8606091f6e2166bb11368a5d8af
Nicola DiPalma <nickyd242@gmail.com>
2014-10-12 23:54:33 -0700

    Added class containing viewport titles.

---
+++ src/app/martiffviewport/ViewportModes.java
@@ -0,0 +1,15 @@
+package app.martiffviewport;
+
+/**
+ * Created by quantumjockey on 10/12/14.
+ */
+public class ViewportModes {
+
+    /////////// Constants ///////////////////////////////////////////////////////////////////
+
+    public static final String VIEWER_OPERATION = "Image Viewer";
+    public static final String SUBTRACTION_OPERATION = "Subtracted Image";
+    public static final String MASK_OPERATION = "Masked Image";
+    public static final String RESULT_OPERATION = "Resultant Image";
+
+}
