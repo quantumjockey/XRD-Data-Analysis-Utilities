416f39a4a33d5b082fe1f5c878ad66b0b5074197
Nicola DiPalma <nickyd242@gmail.com>
2014-10-16 12:47:40 -0700

    Added rendering with mask bounds.

--- src/xrdtiffvisualization/MARTiffVisualizer.java
+++ src/xrdtiffvisualization/MARTiffVisualizer.java
@@ -6,6 +6,7 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import xrdtiffoperations.imagemodel.martiff.MARTiffImage;
 import xrdtiffvisualization.colorramps.GradientRamp;
+import xrdtiffvisualization.masking.BoundedMask;
 
 import java.io.IOException;
 
@@ -29,6 +30,12 @@ public class MARTiffVisualizer {
         return displayed;
     }
 
+    public Image RenderDataAsImage(GradientRamp _ramp, BoundedMask mask) throws IOException {
+        WritableImage displayed = new WritableImage(data.intensityMap[0].length, data.intensityMap.length);
+        RenderImageWithMask(displayed.getPixelWriter(), data.GetMaxValue(), _ramp, mask);
+        return displayed;
+    }
+
     /////////// Private Methods ///////////////////////////////////////////////////////////////
 
     private void RenderImageViaColorRamp(PixelWriter writer, short maxValue, GradientRamp ramp) throws IOException{
@@ -45,6 +52,29 @@ public class MARTiffVisualizer {
                 int value = data.intensityMap[y][x] + VALUE_OFFSET;
                 if (value < 0) {
                     writer.setColor(x, y, Color.RED);
+                }
+                else {
+                    double coefficient = (double)value / (double)(maxValue + VALUE_OFFSET);
+                    writer.setColor(x, y, colorRamp.getRampColorValue(coefficient, 0.0, 1.0));
+                }
+            }
+        }
+    }
+
+    private void RenderImageWithMask(PixelWriter writer, short maxValue, GradientRamp ramp, BoundedMask mask) throws IOException{
+        GradientRamp colorRamp;
+        if (ramp == null) {
+            Color[] ramp_colors = {Color.BLACK, Color.VIOLET, Color.BLUE, Color.GREEN, Color.YELLOW, Color.ORANGE}; // "Spectrum" Ramp
+            colorRamp = new GradientRamp(ramp_colors);
+        }
+        else{
+            colorRamp = ramp;
+        }
+        for (int x = 0; x < data.intensityMap[0].length; x++) {
+            for (int y = 0; y < data.intensityMap.length; y++) {
+                int value = data.intensityMap[y][x] + VALUE_OFFSET;
+                if (value < mask.lowerBound || value > mask.upperBound){
+                    writer.setColor(x, y, mask.maskHue);
                 }
                 else {
                     double coefficient = (double)value / (double)(maxValue + VALUE_OFFSET);
