88a9593174f1b67a8b05f97b5513ab5451a1f105
Nicola DiPalma <nickyd242@gmail.com>
2014-10-22 20:33:53 -0700

    Created classes for field type/tag values.

---
+++ src/xrdtiffoperations/imagemodel/ifd/fields/FieldTypes.java
@@ -0,0 +1,14 @@
+package xrdtiffoperations.imagemodel.ifd.fields;
+
+public class FieldTypes {
+
+    /////////// Constants ///////////////////////////////////////////////////////////////////
+
+    public static int EIGHT_BIT_UNSIGNED_INT = 1;
+    public static int ASCII = 2;
+    public static int SIXTEEN_BIT_UNSIGNED_INT = 3;
+    public static int THIRTY_TWO_BIT_UNSIGNED_INT = 4;
+    public static int RATIONAL = 5;
+
+}
+
