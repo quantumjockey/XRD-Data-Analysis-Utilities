f1be1a57b3fb0423f51bb38aa9d8353e37f56999
Nicola DiPalma <nickyd242@gmail.com>
2014-10-28 22:35:08 -0700

    Removed control-specific functions from MainWindow.

--- src/app/mainwindow/MainWindowController.java
+++ src/app/mainwindow/MainWindowController.java
@@ -1,6 +1,7 @@
 package app.mainwindow;
 
 import app.workspaces.multipleimagesubtractor.MultipleImageSubtractor;
+import app.workspaces.singleimagesubtractor.SingleImageSubtractor;
 import filesystembase.dialogwrappers.DirectoryChooserWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.scene.control.*;
@@ -21,34 +22,10 @@ public class MainWindowController extends WindowControllerBase {
     /////////// Fields ////////////////////////////////////////////////////////////////////////
 
     @FXML
-    private MARTiffViewport selectedImageViewport;
-
-    @FXML
-    private MARTiffViewport subtractedImageViewport;
-
-    @FXML
-    private MARTiffViewport resultantImageViewport;
-
-    // Single-Image Subtraction
-
-    @FXML
-    private ComboBox<String> selectedPath;
-
-    @FXML
-    private ComboBox<String> subtractedPath;
-
-    @FXML
-    private Label rootPath;
-
-    // Multiple-Image Subtraction
-
-    @FXML
     private MultipleImageSubtractor multipleImageWorkspace;
 
-    // Tools Area
-
     @FXML
-    private Accordion toolsContainer;
+    private SingleImageSubtractor singleImageWorkspace;
 
     private ArrayList<PathWrapper> availableFiles;
     private File selectedDirectory;
@@ -57,9 +34,7 @@ public class MainWindowController extends WindowControllerBase {
 
     public MainWindowController() {
         super();
-        selectedImageViewport = new MARTiffViewport();
-        subtractedImageViewport = new MARTiffViewport();
-        resultantImageViewport = new MARTiffViewport();
+        singleImageWorkspace = new SingleImageSubtractor();
         multipleImageWorkspace = new MultipleImageSubtractor();
     }
 
@@ -76,38 +51,14 @@ public class MainWindowController extends WindowControllerBase {
         selectedDirectory = dialog.getSelectedDirectory();
         if (selectedDirectory != null) {
             availableFiles = parseSelectedDirectory();
-            populateControls();
             String path = selectedDirectory.getPath();
-            LabelExt.update(rootPath, path, path);
             multipleImageWorkspace.getController().updateControls(availableFiles, path);
-            try{
-                selectedImageViewport.renderImageFromFile(availableFiles.get(selectedPath.getSelectionModel().getSelectedIndex()));
-                subtractedImageViewport.renderImageFromFile(availableFiles.get(subtractedPath.getSelectionModel().getSelectedIndex()));
-                subtractImages();
-            }
-            catch (IOException ex){
-                System.out.println("Image file could not be rendered!");
-            }
+            singleImageWorkspace.getController().updateControls(availableFiles, path);
         }
     }
 
     /////////// Private Methods ///////////////////////////////////////////////////////////////
 
-    private ChangeListener<String> createListener(ComboBox<String> selector, MARTiffViewport imageViewport) {
-        return (observable, oldValue, newValue) -> {
-            try {
-                SingleSelectionModel selected = selector.getSelectionModel();
-                if (selected.getSelectedIndex() >= 0) {
-                    selector.setTooltip(new Tooltip(selector.getSelectionModel().getSelectedItem()));
-                    imageViewport.renderImageFromFile(availableFiles.get(selected.getSelectedIndex()));
-                    subtractImages();
-                }
-            }
-            catch (IOException ex){
-                System.out.println("Image file could not be read!");
-            }
-        };
-    }
 
     private ArrayList<PathWrapper> parseSelectedDirectory(){
         FilterWrapper tiffFilter = new FilterWrapper(new String[]{".tif", ".tiff"});
@@ -120,36 +71,11 @@ public class MainWindowController extends WindowControllerBase {
         return imagesPaths;
     }
 
-    private void populateControls(){
-        ArrayList<String> temp = new ArrayList<>();
-        availableFiles.forEach((item) -> temp.add(item.getPathTail()));
-
-        // Single Image Subtraction
-        ChangeListener<String> selectedChanged = createListener(selectedPath, selectedImageViewport);
-        ComboBoxExt.populate(selectedPath, temp, selectedChanged);
-        ChangeListener<String> subtractedChanged = createListener(subtractedPath, subtractedImageViewport);
-        ComboBoxExt.populate(subtractedPath, temp, subtractedChanged);
-    }
-
-    private void setDefaultToolAssortment(){
-        toolsContainer.setExpandedPane(toolsContainer.getPanes().get(0));
-    }
-
-    private void subtractImages() throws IOException{
-        MARTiffImage resultantImage = DataSubtraction.subtractImages(selectedImageViewport.getController().getCachedImage(), subtractedImageViewport.getController().getCachedImage());
-        resultantImageViewport.renderImage(resultantImage);
-    }
-
     /////////// Protected Methods /////////////////////////////////////////////////////////////
 
     @Override
     protected void performInitializationTasks(){
-        String rootDefault = "(Unspecified)";
-        selectedImageViewport.getController().setViewportTitle("Selected Image");
-        subtractedImageViewport.getController().setViewportTitle("Subtracted Image");
-        resultantImageViewport.getController().setViewportTitle("Resultant Image");
-        setDefaultToolAssortment();
-        LabelExt.update(rootPath, rootDefault, null);
+
     }
 
 }
