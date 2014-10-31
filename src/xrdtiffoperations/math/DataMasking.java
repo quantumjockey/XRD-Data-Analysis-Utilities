b6eb1bd4f88522dfa4a1077ee2a77b38cc9dded0
quantumjockey <nickyd242@gmail.com>
2014-10-30 22:39:45 -0700

    Changed name(s) generated upon image mask.

--- src/xrdtiffoperations/math/DataMasking.java
+++ src/xrdtiffoperations/math/DataMasking.java
@@ -5,11 +5,15 @@ import xrdtiffoperations.imagemodel.martiff.WritableMARTiffImage;
 
 public class DataMasking {
 
+    /////////// Constants /////////////////////////////////////////////////////////////////////
+
+    private static final String DEFAULT_EXTENSION = ".tif";
+
     /////////// Public Methods ////////////////////////////////////////////////////////////////
 
     public static MARTiffImage maskImage(MARTiffImage image, int minVal, int maxVal){
         WritableMARTiffImage temp = (WritableMARTiffImage)image;
-        String name = temp.getFilename().replace('.', '-') + "_mask_lb" + minVal + "_ub" + maxVal + ".tif";
+        String name = generateFilename(image, minVal, maxVal);
         temp.setFilename(name);
         for(int y = 0; y < temp.getHeight(); y++){
             for (int x = 0; x < temp.getWidth(); x++){
@@ -21,6 +25,21 @@ public class DataMasking {
 
     /////////// Private Methods ///////////////////////////////////////////////////////////////
 
+    private static String generateFilename(MARTiffImage file, int min, int max){
+        String result;
+        String baseName = stripFilename(file.getFilename());
+        if (max != file.getMaxValue()){
+            result = baseName + "_max_" + max + DEFAULT_EXTENSION;
+        }
+        else if (min != file.getMinValue()){
+            result = baseName + "_min_" + min + DEFAULT_EXTENSION;
+        }
+        else{
+            result = baseName + DEFAULT_EXTENSION;
+        }
+        return result;
+    }
+
     private static short maskValue(short _value, short _max, short _min){
         short value = _value;
         if (value < _min){
@@ -30,6 +49,10 @@ public class DataMasking {
             value = _max;
         }
         return value;
+    }
+
+    private static String stripFilename(String name){
+        return name.replace(DEFAULT_EXTENSION, "");
     }
 
 }
