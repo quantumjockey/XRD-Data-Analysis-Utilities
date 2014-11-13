4528b51e4afb317be071dacb9057a3bb94313e8b
Nicola DiPalma <nickyd242@gmail.com>
2014-11-13 12:09:27 -0800

    Replaced ComboBoxes with TreeView selectors.

--- src/app/workspaces/singleimagecorrection/components/SingleImageSubtractorController.java
+++ src/app/workspaces/singleimagecorrection/components/SingleImageSubtractorController.java
@@ -1,6 +1,7 @@
 package app.workspaces.singleimagecorrection.components;
 
 import app.controls.martiffviewport.MARTiffViewport;
+import mvvmbase.controls.macros.TreeViewExt;
 import paths.PathWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
@@ -28,10 +29,10 @@ public class SingleImageSubtractorController extends MarkupControllerBase {
     private MARTiffViewport resultantImageViewport;
 
     @FXML
-    private ComboBox<String> selectedPath;
+    private TreeView<String> selectedPath;
 
     @FXML
-    private ComboBox<String> subtractedPath;
+    private TreeView<String> subtractedPath;
 
     @FXML
     private Label rootPath;
@@ -44,10 +45,12 @@ public class SingleImageSubtractorController extends MarkupControllerBase {
         availableFiles = newItems;
         ArrayList<String> temp = new ArrayList<>();
         availableFiles.forEach((item) -> temp.add(item.getPathTail()));
-        ChangeListener<String> selectedChanged = createListener(selectedPath, selectedImageViewport);
-        ComboBoxExt.populate(selectedPath, temp, false, selectedChanged);
-        ChangeListener<String> subtractedChanged = createListener(subtractedPath, subtractedImageViewport);
-        ComboBoxExt.populate(subtractedPath, temp, false, subtractedChanged);
+        ChangeListener<TreeItem<String>> selectedChanged = createListener(selectedPath, selectedImageViewport);
+        TreeViewExt.populateTree(selectedPath, temp, root, SelectionMode.SINGLE, false, null);
+        selectedPath.getSelectionModel().selectedItemProperty().addListener(selectedChanged);
+        ChangeListener<TreeItem<String>> subtractedChanged = createListener(subtractedPath, subtractedImageViewport);
+        TreeViewExt.populateTree(subtractedPath, temp, root, SelectionMode.SINGLE, false, null);
+        subtractedPath.getSelectionModel().selectedItemProperty().addListener(subtractedChanged);
         LabelExt.update(rootPath, root, root);
         try{
             selectedImageViewport.renderImageFromFile(availableFiles.get(selectedPath.getSelectionModel().getSelectedIndex()));
@@ -61,13 +64,16 @@ public class SingleImageSubtractorController extends MarkupControllerBase {
 
     /////////// Private Methods ///////////////////////////////////////////////////////////////
 
-    private ChangeListener<String> createListener(ComboBox<String> selector, MARTiffViewport imageViewport) {
+    private ChangeListener<TreeItem<String>> createListener(TreeView<String> selector, MARTiffViewport imageViewport) {
         return (observable, oldValue, newValue) -> {
             try {
-                SingleSelectionModel selected = selector.getSelectionModel();
-                if (selected.getSelectedIndex() >= 0) {
-                    selector.setTooltip(new Tooltip(selector.getSelectionModel().getSelectedItem()));
-                    imageViewport.renderImageFromFile(availableFiles.get(selected.getSelectedIndex()));
+                MultipleSelectionModel selected = selector.getSelectionModel();
+                if (!selectedPath.getSelectionModel().isEmpty()
+                        && newValue.isLeaf()
+                        && selected.getSelectedIndex() >= 0){
+                    String tip = "Current Selection: " + selector.getSelectionModel().getSelectedItem().getValue();
+                    selector.setTooltip(new Tooltip(tip));
+                    imageViewport.renderImageFromFile(getPath(newValue.getValue()));
                     subtractImages();
                 }
             }
@@ -75,6 +81,17 @@ public class SingleImageSubtractorController extends MarkupControllerBase {
                 System.out.println("Image file could not be read!");
             }
         };
+    }
+
+    private PathWrapper getPath(String file){
+        PathWrapper path = null;
+        for (PathWrapper item : availableFiles){
+            if (item.getPathTail().equals(file)){
+                path = item;
+                break;
+            }
+        }
+        return path;
     }
 
     private void subtractImages() throws IOException{
