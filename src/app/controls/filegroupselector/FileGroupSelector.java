9ab8befdf61199563710ea4168720f96294a7093
Nicola DiPalma <nickyd242@gmail.com>
2014-11-22 20:07:28 -0800

    Drafted selector control for picking files in a TreeView.

---
+++ src/app/controls/filegroupselector/FileGroupSelector.java
@@ -0,0 +1,37 @@
+package app.controls.filegroupselector;
+
+import app.controls.filegroupselector.components.FileGroupSelectorController;
+import javafx.scene.layout.VBox;
+import mvvmbase.markup.initialization.MarkupInitializer;
+import mvvmbase.markup.initialization.MarkupInitializerMacro;
+
+public class FileGroupSelector extends VBox {
+
+    /////////// Constants ///////////////////////////////////////////////////////////////////
+
+    private final String MARKUP_FILE = "FileGroupSelectorView.fxml";
+
+    /////////// Fields //////////////////////////////////////////////////////////////////////
+
+    private MarkupInitializer markupContainer;
+
+    /////////// Constructors ////////////////////////////////////////////////////////////////
+
+    public FileGroupSelector() {
+        initializeComponents();
+    }
+
+    /////////// Public Methods //////////////////////////////////////////////////////////////
+
+    public FileGroupSelectorController getController(){
+        return (FileGroupSelectorController)markupContainer.getController();
+    }
+
+    /////////// Private Methods /////////////////////////////////////////////////////////////
+
+    private void initializeComponents(){
+        markupContainer = MarkupInitializerMacro.createInitializer(this, FileGroupSelectorController.class, MARKUP_FILE);
+        markupContainer.load();
+    }
+
+}
