fddc2ead59da0db58e6ee15a612db1bf07cc5b0c
quantumjockey <nickyd242@gmail.com>
2015-02-15 01:58:17 -0800

    Modified IO classes for dealing with generic frame data.

--- src/app/filesystem/FileSysReader.java
+++ src/app/filesystem/FileSysReader.java
@@ -1,6 +1,7 @@
 package app.filesystem;
 
 import com.quantumjockey.paths.PathWrapper;
+import xrdtiffoperations.data.DiffractionFrame;
 import xrdtiffoperations.imagemodel.martiff.MARTiffImage;
 import xrdtiffoperations.filehandling.io.TiffReader;
 import java.io.IOException;
@@ -10,14 +11,14 @@ public class FileSysReader {
 
     /////////// Public Methods //////////////////////////////////////////////////////////////
 
-    public static MARTiffImage readImageData(PathWrapper imagePath) throws IOException {
+    public static DiffractionFrame readImageData(PathWrapper imagePath) throws IOException {
         MARTiffImage temp = null;
         if (imagePath != null) {
             TiffReader marImageReader = new TiffReader(Paths.get(imagePath.getInjectedPath()));
             marImageReader.readFileData();
             temp = marImageReader.getImageData();
         }
-        return temp;
+        return temp.getDiffractionData();
     }
 
 }
--- src/app/filesystem/FileSysWriter.java
+++ src/app/filesystem/FileSysWriter.java
@@ -1,5 +1,6 @@
 package app.filesystem;
 
+import xrdtiffoperations.data.DiffractionFrame;
 import xrdtiffoperations.imagemodel.martiff.MARTiffImage;
 import xrdtiffoperations.filehandling.io.TiffWriter;
 import java.io.File;
@@ -8,9 +9,9 @@ public class FileSysWriter {
 
     /////////// Public Methods //////////////////////////////////////////////////////////////
 
-    public static void writeImageData(File path, MARTiffImage image, String type){
+    public static void writeImageData(File path, DiffractionFrame image, String type){
         if (path != null) {
-            TiffWriter writer = new TiffWriter(image, type);
+            TiffWriter writer = new TiffWriter(new MARTiffImage(image), type);
             writer.write(path.getPath());
         }
     }
