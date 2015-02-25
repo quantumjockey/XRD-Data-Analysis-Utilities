35791a7bd0f81633904f9b1ff41e896492a94119
quantumjockey <nickyd242@gmail.com>
2015-02-25 01:53:38 -0800

    Added alert to indicate faulty folder selection.

--- src/main/java/app/mainwindow/MainWindowController.java
+++ src/main/java/app/mainwindow/MainWindowController.java
@@ -4,8 +4,10 @@ import app.workspaces.bulkimagecorrection.BulkImageCorrection;
 import app.workspaces.singleimagecorrection.SingleImageCorrection;
 import app.workspaces.singleimageviewer.SingleImageViewer;
 import com.quantumjockey.dialogs.DirectoryChooserWrapper;
+import com.quantumjockey.mvvmbase.dialogs.AlertWindow;
 import com.quantumjockey.mvvmbase.window.WindowControllerBase;
 import com.quantumjockey.parsers.DirectoryParser;
+import com.quantumjockey.paths.SystemAttributes;
 import javafx.fxml.FXML;
 import com.quantumjockey.paths.PathWrapper;
 import com.quantumjockey.paths.filters.FilterWrapper;
@@ -42,11 +44,21 @@ public class MainWindowController extends WindowControllerBase {
         File selectedDirectory = dialog.getSelectedDirectory();
 
         if (selectedDirectory != null) {
+
             availableFiles = DirectoryParser.parseSelectedDirectory(selectedDirectory, new FilterWrapper(new String[]{FileExtensions.DEFAULT, FileExtensions.EXTENDED_DEFAULT, FileExtensions.MAR_2300, FileExtensions.MAR_3450}));
-            String path = selectedDirectory.getPath();
-            multipleImageWorkspace.getController().updateControls(availableFiles, path);
-            singleImageWorkspace.getController().updateControls(availableFiles, path);
-            singleImageViewer.getController().updateControls(availableFiles, path);
+
+            if (availableFiles.size() != 0) {
+                String path = selectedDirectory.getPath();
+                multipleImageWorkspace.getController().updateControls(availableFiles, path);
+                singleImageWorkspace.getController().updateControls(availableFiles, path);
+                singleImageViewer.getController().updateControls(availableFiles, path);
+            } else {
+                String title = "No Files Found";
+                String message = "No files matching XRD file extension filters have been found in this directory. If files exist in a subdirectory of this   " + SystemAttributes.LINE_SEPARATOR
+                        + "folder, that subdirectory must be explicitly selected in order for files to be parsed appropriately.";
+                AlertWindow emptyDirectoryAlert = new AlertWindow(title, message);
+                emptyDirectoryAlert.show();
+            }
         }
     }
 
